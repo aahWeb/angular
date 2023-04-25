3e24960db09ea0f9382bd2ac7e73fd79b7ab92c2
Antoine07 <antoine.lucsko@gmail.com>
2023-04-25 10:32:10 +0200

    queue generique

--- SEMAINE_01/J1_2_intro_typescript/Example/04_Type_Gen.ts
+++ SEMAINE_01/J1_2_intro_typescript/Example/04_Type_Gen.ts
@@ -8,8 +8,8 @@ console.log(c); // affichera ['a', 'b', 'c', 'd', 'e']
 
 fusion<number>([1, 2, 3], [5, 6, 7]);
 
-function fusionTR<T, R>(a: T[], b: R[]): any[] {
-  const res: any[] = [];
+function fusionTR<T, R>(a: T[], b: R[]): Array<T|R> {
+  const res: Array<T|R> = [];
 
   return res.concat(a, b);
 }
