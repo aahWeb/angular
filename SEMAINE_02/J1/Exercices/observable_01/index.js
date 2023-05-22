680c86aa096c18ce647af94bcd95d1b4a5e2c91d
Antoine07 <antoine.lucsko@gmail.com>
2023-05-22 16:27:22 +0200

    rxjs

---
+++ SEMAINE_02/J1/Exercices/observable_01/index.js
@@ -0,0 +1,26 @@
+import { Observable, filter } from "rxjs"
+import { map } from "rxjs/operators"
+
+const observable = new Observable((observer) => {
+    observer.next(1);
+    observer.next(2);
+    // observer.next(3);
+    let count = 3;
+    let interval = setInterval(() => {
+    // ++count incrément avant de retourner la valeur dans la fonction next
+      observer.next(++count);
+
+      if(count > 10) observer.complete();
+
+    }, 1000);
+  });
+
+// consommer 
+// observable.subscribe(console.log)
+
+const pipeNumbers = observable.pipe(
+    map(number => number + 10),
+    filter(number => number % 2)
+);
+
+pipeNumbers.subscribe(console.log)
---
+++ SEMAINE_02/J1/Exercices/observable_01/readme.md
@@ -0,0 +1,25 @@
+# Installation Rxjs
+
+On crée le dossier observable_01
+
+Dans le dossier 
+
+## Configuration minimale
+
+```bash
+npm init -y
+```
+
+## installation de RxJS dans notre projet
+
+```bash
+npm i rxjs
+```
+
+Dans le fichier package.json
+
+```json
+  "scripts": {
+    "start": "nodemon index.js"
+  },
+```
